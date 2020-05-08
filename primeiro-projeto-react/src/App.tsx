808e799c0316419b6799ce4344615188f8c45167
Luiz Doreto <luiz.doreto@icloud.com>
2020-05-07 23:21:54 -0300

    Configuring Routes and Styled components

--- primeiro-projeto-react/src/App.tsx
+++ primeiro-projeto-react/src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
-  return <h1>Ola mundoo</h1>;
-}
+import GlobaStyle from './styles/global';
+import Routes from './routes';
+
+const App: React.FC = () => (
+  <>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    <GlobaStyle />
+  </>
+);
 
 export default App;
