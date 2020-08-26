b98e5b75c9d13aec0ab51ddcf3a5e063767e01af
Luiz Doreto <luiz.doreto@icloud.com>
2020-08-25 23:35:52 -0300

    Input style and unform integration

--- gobarber-web/src/pages/SignUp/index.tsx
+++ gobarber-web/src/pages/SignUp/index.tsx
@@ -1,37 +1,58 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
 
 import logo from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
-const SignUp: React.FC = () => (
-  <Container>
-    <Background />
-    <Content>
-      <img src={logo} alt="GoBarber" />
+const SignUp: React.FC = () => {
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Field required'),
+        email: Yup.string().required('Field required').email('Invalid email'),
+        password: Yup.string().min(
+          6,
+          'Password must have at least 6 characters',
+        ),
+      });
 
-      <form>
-        <h1>Register</h1>
-        <Input name="name" icon={FiUser} placeholder="Name" />
-        <Input name="email" icon={FiMail} placeholder="Email" />
-        <Input
-          name="password"
-          icon={FiLock}
-          placeholder="Password"
-          type="password"
-        />
+      await schema.validate(data, { abortEarly: false });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
-        <Button type="submit">Register</Button>
-      </form>
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logo} alt="GoBarber" />
 
-      <a href="newAccount">
-        <FiArrowLeft />
-        Back to logon
-      </a>
-    </Content>
-  </Container>
-);
+        <Form onSubmit={handleSubmit}>
+          <h1>Register</h1>
+          <Input name="name" icon={FiUser} placeholder="Name" />
+          <Input name="email" icon={FiMail} placeholder="Email" />
+          <Input
+            name="password"
+            icon={FiLock}
+            placeholder="Password"
+            type="password"
+          />
+
+          <Button type="submit">Register</Button>
+        </Form>
+
+        <a href="newAccount">
+          <FiArrowLeft />
+          Back to logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
 
 export default SignUp;
